b0141c2645b33471fea1cf1c10ace72b2bc07e07
Henry Lin <henrylin@csilvm-01.cs.ucsb.edu>
2020-11-02 21:56:55 -0800

    lab04

--- lab04/arrays.cpp
+++ lab04/arrays.cpp
@@ -32,7 +32,6 @@ int main( )
 
   // PUT MISSING CODE HERE
   print_array(array, size);
-  cout << endl;
 
   int max = maxArray(array, size);
   cout << "Max = " << max << endl;
@@ -46,15 +45,22 @@ int main( )
   cout << "Evens: "; evensArray(array, size);
   
   cout << "Primes: "; primesArray(array, size); 
-   
+
+  cout << "Searches: " << endl;
+  
+  AllSearches(array, size);
+  cout << endl;
+  
   return 0;
 }
 
 
 void print_array(int arr[], int asize){
-  for(int i = 0; i < asize; i++)
-    if(arr[i] < (asize-1))
+  for(int i = 0; i < asize-1; i++){
       cout << arr[i] << ", ";
+  }
+  cout << arr[asize-1] << endl;
+  
   
 }
 
@@ -81,16 +87,16 @@ int sumArray(int arr[], int asize){
 
 void evensArray(int arr[], int asize){
   for (int i = 0; i < asize; i++)
-    if(arr[i]%2==0)
+    if(arr[i]%2==0) // Even number should have no remainders
       cout << arr[i] << ", ";
   cout << "end" << endl;
 }
 
 void primesArray(int arr[], int asize){
-  for(int i = 0; i<asize; i++){
-    int x = 1;
-    if(arr[i] > 1){
-      for (int j = 2; j <= arr[i]/2; ++j){
+  for(int i = 0; i<asize; i++){ 
+    int x = 1;                           // declare variable x = 1
+    if(arr[i] > 1){                      // prime numbers cannot be negative or 0 or 1
+      for (int j = 2; j <= arr[i]/2; j++){  //prime number starts with 2, if the number%j == 0, it is not a prime number
         if(arr[i]%j == 0){
           x = 0;
           break;
@@ -108,13 +114,19 @@ void primesArray(int arr[], int asize){
 
 
 int SeqSearch(int arr[], int array_size, int target){
-  int index = 0;
   for (int i = 0; i < array_size ; i++)
-    if (arr[i] == target)
-      index = i;
-    else
-      index = -1;
-  return index;
+    if (arr[i] == target) // if the target is in the array, then the function would return the index of the number. 
+      return i;
+  return -1; // if not the function will return -1, which the lab instructions told us to do so. 
 }
 void AllSearches( int array[], int array_size){
+  int sequent;
+  for(int i = 0; i < NSEARCHES; i++){              // i increase until NSEARCHES 10 in constants. 
+    cout << "Looking for " << SEARCHES[i] << ". "; // Searches[] array declared in constants 
+    sequent = SeqSearch(array, array_size, SEARCHES[i]);            //sequent search called, either returns index i or -1
+    if(sequent != -1)                       // if index is not = 1 then it is found, and the index of the found number position from arr[] would be printed
+      cout << "Found at index: " << sequent << endl;
+    else               // if i==-1, which is from the seqsearch, then the target is not found in the Array
+      cout << "Nout Found!" << endl;
+  }
 }
